6c71af32782d395b7793168b4a835716bf3afbb9
Adrien Perez <adrien.perez789@gmail.com>
2019-10-22 09:56:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Bicycle.php';
+
+require_once 'Car.php';
+
+require_once 'Truck.php';
+
+$bigtruck = new Truck('green', 4, 'fuel', 10);
+
+var_dump($bigtruck);
+
+$bigtruck
+    ->setEnergy( 'electric')
+    ->setCargo(90);
+echo $bigtruck->filling();
